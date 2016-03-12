951f34bd4aaa17d83c874ad5089d5d0abd54ba1d
Margaret <margareth.florian@gmail.com>
2016-03-12 16:46:27 -0500

    Added files via upload

---
+++ resources/views/person_memories.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.user')
+
+@section('js')
+<script src="../js/person_memories.min.js" type="text/javascript"></script>
+@endsection
+
+@section('content')
+
+  <section class="intro-section center">       
+    <p><strong>Dashboard<br /></strong></p>        
+    <nav>
+      <?php foreach($buddies_data->buddy as $key=>$value){ ?>
+        <p>
+            <a href="javascript:void(0);">
+                <img src="../images/avatars/<?php echo $value->avatar; ?>" /><?php echo $value->buddy_nickname." (".$value->compatibility." % compatibilidad)"; ?></a>
+         </p>
+      <?php } ?>                    
+     </nav>                
+                
+      <div class="content-section">
+        <form name="new_person_form" id="new-person-form" action="" method="POST">
+        <fieldgroup>          
+            <div class="fieldinput">
+                <input type="text" name="person_nickname" id="person-nickname" maxlength="25" value="" placeholder="Cómo llamas a esta persona? *" data-rule-required="true" data-msg-required="Cómo le llamas a la persona que quieres agregar?." data-rule-maxlength="25" data-msg-maxlength="No puedes exceder los 15 caracteres" />
+            </div>
+        </fieldgroup>    
+        <fieldgroup>          
+            <div class="fieldinput">                            
+                        <select data-rule-required="true" data-msg-required="Selecciona el género de la persona.">
+                            <option value="">Género *</option>
+                         <?php foreach($new_person_data['genders'] as $key=>$value){ ?>
+                            <option value="<?php echo $key; ?>"><?php echo $value; ?></option>
+                         <?php } ?>                        
+                        </select>
+            </div>
+        </fieldgroup>    
+        <fieldgroup>          
+            <div class="fieldinput">                        
+                        <br/>Selecciona un Avatar para esta persona:<br/>                        
+                        <?php foreach($new_person_data['avatars'] as $key=>$value){ ?>
+                            <input type="radio" name="person_avatar" id="person-avatar" value="<?php echo $key; ?>" /><img src="../images/avatars/<?php echo $value; ?>" />                        
+                         <?php } ?>                                      
+            </div>
+        </fieldgroup>    
+        <div class="button-section">
+              <input type="submit" name="add_person_button" id="add-person-button" value="Adicionar Persona" />                    
+        </div>         
+       </form>   
+     </div>          
+     <div id="new-person-messages"></div>
+            
+@endsection
+<script>
+    var validation = "<?php echo $session_id; ?>";
+</script>
---
+++ resources/views/visitor.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.visitor')
+
+@section('content')
+  <section class="intro-section center">
+    <h1><img src="images/keep-app-logo.png" width="87" height="91"></h1>
+    <!--<svg class="icon"><use xlink:href="#..." /></svg>-->
+    <p>
+      <strong>Bienvenido a Keep Memories<br /></strong>
+      Un espacio donde podr&aacute;s crear recuerdos e inter&eacute;ses que tengas en comun con cualquier persona de forma gratis y privada.
+    </p>
+    <div class="register-log-menu">
+      <a href="javascript:void(0);" id="signup-link">Crea una cuenta</a>
+      <a href="javascript:void(0);" class="active" id="signin-link">Ingresa</a>
+    </div>
+  </section>
+  <section class="homeLog-section">
+          
+    <div id="signup-signin-messages"></div>
+    <div class="content-section" id="singin-div-form">       
+       <form name="mem_user_signin_form" id="mem-user-signin-form" action="" method="POST">
+          <fieldgroup>          
+            <div class="fieldinput">          
+              <input type="email" name="user_email" id="user-email" value="" placeholder="Correo Electr&oacute;nico" data-rule-required="true" data-rule-email="true" data-msg-required="Ingresa tu Correo Electr&oacute;nico." data-msg-email="Ingresa un Correo Electr&oacute;nico válido." />              
+            </div>
+            <div class="fieldinput">
+              <input type="password" name="user_pswd" id="user-pswd" value="" placeholder="Contraseña" autocomplete="off" data-rule-required="true" data-msg-required="Ingresa tu contraseña." />
+            </div>        
+          </fieldgroup>       
+            <div class="button-section">
+              <input type="submit" name="login_button" id="login-button" value="Ingresar" />
+            </div>
+       </form>
+    </div>
+    
+    <div class="content-section" id="singup-div-form" style="display: none">
+       <form name="mem_user_signup_form" id="mem-user-signup-form" method="POST">
+          <fieldgroup>          
+            <div class="fieldinput">          
+              <input type="email" name="user_email" id="user-email" placeholder="Correo Electr&oacute;nico" data-rule-required="true" data-rule-email="true" data-msg-required="Ingresa tu Correo Electr&oacute;nico." data-msg-email="Ingresa un Correo Electr&oacute;nico válido." />              
+            </div>
+            <div class="fieldinput">
+              <input type="password" name="user_pswd" id="signup-user-pswd" placeholder="Contraseña" autocomplete="off" data-rule-required="true" data-msg-required="Ingresa una Contraseña." />
+            </div>
+            <div class="fieldinput">
+              <input type="password" name="user_pswd_confirm" id="user-pswd-confirm" placeholder="Confirma tu Contraseña" autocomplete="off" data-rule-required="true" data-msg-required="Confirma tu contraseña." data-rule-equalto="#signup-user-pswd" data-msg-equalto="La contrase&ntilde;a no coincide." />              
+            </div>
+          </fieldgroup> 
+          <fieldgroup>
+            <div class="facebook-log center">
+                <a href="">Registrate con facebook</a>
+                <p>No te preocupes, nosotros no utilizaremos, ni compartiremos informaci&oacute;n en tu muro de Facebook.</p>
+            </div>
+            <div class="agreement">
+                <a href="javascript:void(0);"><svg class="icon-check"><use xlink:href="#check" /></svg>Estoy deacuerdo con los t&eacute;rminos de uso y pol&iacute;ticas de privacidad.</a>
+            </div>
+          </fieldgroup>
+            <div class="button-section">
+              <input type="submit" name="signup_button" id="signup-button" value="Registrarme" />
+            </div>
+      </form>
+  </div>
+  </section>
+@endsection
