d3a7715ac187ae9971e2aa35aab315c10ef4727b
Alvaro Rojas Tafur <arojastafur@gmail.com>
2016-03-22 12:00:38 -0500

    css fixes

---
+++ storage/framework/views/2bdc14b5de6a130e1d2b641c79e78890c8d99ebb.php
@@ -0,0 +1,88 @@
+<?php $__env->startSection('js'); ?>
+<script src="<?php echo CC_JS_IMG; ?>js/dashboard.min.js" type="text/javascript"></script>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+<!--PAGES MY LIST-->
+  <section class="pages-section my-list">
+    <div class="section-full-border title-main-section">
+      <div class="content-section">
+        <h2>Mi Lista</h2>
+      </div>
+    </div>
+    <section class="people-list">            
+    <?php foreach($buddies_data->buddy as $key=>$value){ ?>         
+         <div class="section-full-border">
+            <div class="content-section">
+              <a class="display-section" href="<?php echo APPURL; ?>person-memories/<?php echo $session_id; ?>/<?php echo $value->buddy_id; ?>">
+                <div class="display-section-avatar">  
+                  <svg class="avatar-<?php echo $value->avatar; ?>"><use <?php echo 'xlink:href="#avatar-'.$value->avatar.'"'; ?> /></svg>  
+                </div>
+                <div class="display-section-info">
+                    <div class="display-section-name">
+                      <?php echo $value->buddy_nickname; ?>
+                    </div>
+                    <div class="display-section-resume">
+                      <span><?php echo $value->num_memories; ?> Recuerdos</span>
+                      <span><?php echo $value->num_interest; ?> Inter&eacute;ses</span>
+                    </div>
+                </div>
+                <?php if($value->compatibility){ ?>
+                <div class="compatibility-item-result comp-positive" href="javascript:void(0);">
+                  <svg class="comp-result-icon"><use xlink:href="#compatible" /></svg> 
+                  <p><?php echo $value->compatibility; ?>%</p> 
+                </div>
+                <?php }else{ ?>
+                <div class="compatibility-item-result comp-non" href="javascript:void(0);">
+                  <span>Ingresa algunos inter&eacute;ses para ver<br /> la compatibilidad con esta persona</span> 
+                </div>
+                <?php } ?>
+              </a>
+            </div>
+          </div>
+         
+         
+    <?php } ?>  
+    </section>
+    <a class="add-btn" id="link-add-new-person" href="javascript:void(0);">
+      <svg class="icon-plus"><use xlink:href="#agregar" /></svg>
+      <p>Agregar una persona</p>
+    </a>
+  </section>
+   
+   <!--PAGES CREATE PERSON-->
+  <section class="pages-section create-person-page" id="new-person-section" style="display: none">
+    <div class="content-section">      
+      <section class="action-box-wrap">        
+        <div class="action-box">           
+              <div class="action-box-title">
+                Nueva Persona
+              </div> 
+              <div class="action-box-info">
+                <form name="new_person_form" id="new-person-form" action="" method="POST">              
+                  <fieldgroup>
+                    <div class="fieldinput">                  
+                        <select name="person_gender" id="new-person-gender" data-rule-required="true" data-msg-required="Selecciona el género de la persona.">
+                             <option value="">Género *</option>
+                          <?php foreach($new_person_data['genders'] as $key=>$value){ ?>
+                                <option value="<?php echo $key; ?>"><?php echo $value; ?></option>
+                          <?php } ?>                        
+                         </select>
+                    </div>
+                    <div class="fieldinput">                  
+                      <input type="text" name="person_nickname" id="new-person-nickname" maxlength="30" value="" placeholder="Cómo deseas llamar a esta persona? *" data-rule-required="true" data-msg-required="Cómo le llamas a la persona que quieres agregar?." data-rule-maxlength="30" data-msg-maxlength="No puedes exceder los 30 caracteres" />
+                      <span class="character-counter">30 caract&eacute;res</span>
+                    </div>
+                  </fieldgroup>
+                  <input type="submit" name="add_person_button" id="add-person-button" value="Crear" />  
+                </form>
+              </div>                         
+        </div>  
+      </section>
+
+    </div>
+  </section>     
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.user', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ storage/framework/views/5011e4a9174908bf22d2b4985d17c87bbd6a8683.php
@@ -0,0 +1,143 @@
+<?php $__env->startSection('js'); ?>
+<script src="<?php echo CC_JS_IMG; ?>js/person_memories.min.js" type="text/javascript"></script>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+
+<!--PAGES PERSON MEMORIES-->
+  <section class="pages-section my-list">
+    <div class="section-full-border title-main-section">
+        <div class="content-section">
+          <a class="display-section" href="">
+            <div class="display-section-avatar">  
+              <svg class="avatar-<?php echo $buddy_data['buddy']->avatar; ?>" style="display:none;"><use  <?php echo 'xlink:href="#avatar-'.$buddy_data['buddy']->avatar.'"'; ?> /></svg>                
+            </div>
+            <div class="display-section-info">
+                <div class="display-section-name">
+                  <?php echo $buddy_data['buddy']->buddy_nickname; ?>
+                </div>
+                <div class="display-section-resume">
+                  <span><?php echo $buddy_data['buddy']->num_memories; ?> Recuerdos</span>
+                  <span><?php echo $buddy_data['buddy']->num_interest; ?> Inter&eacute;ses</span>
+                </div>
+            </div>
+            <div class="compatibility-item-result comp-positive" href="">
+              <svg class="comp-result-icon"><use xlink:href="#compatible" /></svg> 
+              <p><?php echo $buddy_data['buddy']->compatibility; ?>%</p> 
+            </div>
+          </a>
+        </div>
+      </div>
+   <?php if($buddy_total_memories){ 
+        
+          foreach($buddy_memories['memory'] as $key=>$value){       
+   ?>
+      <div class="section-full-border">
+        <div class="content-section">
+          <a class="display-section link-display-mem-resume" href="javascript:void(0);" id="link-display-mem-resume_<?php echo $value->memory_id; ?>">
+            <div class="display-section-avatar">  
+              <svg class="agradable"><use <?php echo 'xlink:href="#'.$value->feeling.'"'; ?> /></svg>               
+            </div>
+            <div class="display-section-info">
+                <div class="display-section-name">
+                  <?php echo $value->title; ?>
+                </div>
+                <div class="show-memory-resume" id="show-memory-resume_<?php echo $value->memory_id; ?>" style="display: none">
+                  <p class="resume-descrip"><?php echo $value->desc; ?></p>
+                  <?php if(count($value->compatibility)){ ?>
+                  <ul class="resume-interest-list">
+                    <h4>Inter&eacute;ses en el recuerdo</h4>
+                    <?php foreach($value->compatibility as $interest=>$percentage) {
+                        $compatibility = ($percentage > 60)?'xlink:href="#compatible"':'xlink:href="#incompatible"';
+                    ?>
+                    <li>
+                      <svg class=""><use <?php echo $compatibility; ?> /></svg> 
+                      <p><?php echo $interest; ?></p>
+                    </li>
+                    <?php } ?>                    
+                  </ul>
+                  <?php } ?> 
+                </div>
+            </div>
+          </a>
+        </div>
+      </div>
+      <?php }//end foreach ?>      
+    <?php } ?>  
+  </section>
+  <a class="add-btn" id="link-add-person-memory" href="javascript:void(0);">
+    <svg class="icon-plus"><use xlink:href="#agregar" /></svg>
+    <p>Agregar Recuerdo</p>
+  </a>
+<!--///-->
+  <section class="action-box-wrap" id="new-person-memory-section" style="display: none">
+        <div class="action-box">
+          <div class="action-box-title">
+            Agrega un recuerdo con <?php echo $buddy_data['buddy']->buddy_nickname; ?>
+          </div> 
+          <div class="action-box-info">
+            <form>
+              <fieldgroup>
+                <div class="fieldinput">
+                  <input name="memory_title" id="memory-title" type="text" value="" placeholder="T&iacute;tulo del Recuerdo">
+                  <span class="character-counter" id="memory-title-charcounter">30 caract&eacute;res</span>
+                </div>
+                <div class="fieldinput">
+                  <textarea name="memory_desc" id="memory-desc" placeholder="Describe tu recuerdo" ></textarea>
+                  <span class="character-counter" id="memory-desc-charcounter">140 caract&eacute;res</span>
+                </div>
+                <div class="fieldinput pick-date">
+                  <label class="label-title">Agrega una fecha:</label>
+                  <a href="javascript:void(0);"><svg class="icon-plus"><use xlink:href="#calendario" /></svg></a>
+                </div>
+                <div class="fieldinput memory-std">
+                  <label class="label-title">Asignale un estado al recuerdo:</label>
+                  <ul class="estado-recuerdo">
+                    <li>
+                      <a href="javascript:void(0);">
+                        <svg class="memory-feeling" id="great"><use xlink:href="#great" /></svg>
+                        <span>me encanta</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="javascript:void(0);">
+                        <svg class="memory-feeling" id="bad"><use xlink:href="#bad" /></svg>
+                        <span>No me gusta</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="javascript:void(0);">
+                        <svg class="memory-feeling" id="sad"><use xlink:href="#sad" /></svg>
+                        <span>Triste</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="javascript:void(0);">
+                        <svg class="memory-feeling" id="nice"><use xlink:href="#nice" /></svg>
+                        <span>me gusta</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="javascript:void(0);">
+                        <svg class="memory-feeling" id="wow"><use xlink:href="#wow" /></svg>
+                        <span>Sorpresa</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="javascript:void(0);">
+                        <svg class="memory-feeling" id="hate"><use xlink:href="#hate" /></svg>
+                        <span>Lo odio</span>
+                      </a>
+                    </li>
+                  </ul> 
+                </div>
+              </fieldgroup>
+              <input name="create_memory" type="submit" value="Crear Recuerdo">
+            </form>
+          </div> 
+        </div>  
+  </section>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.user', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
